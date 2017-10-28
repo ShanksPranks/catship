19dce38e4a144a36b4e9a40bdff942ecaa27a2bf
Shankspranks <jafooli@gmail.com>
2017-10-28 18:18:41 +0200

    new crypto lib
    This library will be used to handle the catship blockchain

---
+++ src/catShipChain.js
@@ -0,0 +1,49 @@
+var catShipChain = [
+	{
+		"blockHeight": 0,
+		"block": {
+			"transactionArray": [
+				{
+					"senderAddress": "catShipCoinBase",
+					"receiverAddress": "04a0ca271b2e123e97001ccf99770d25e23f7b07f736d5f2399ad2ec446d10db1e766d61e3816d84994b99da593a0416c3555bf64a7b676d192b3e78106adfb20c",
+					"message": "freshly minted kitty goodness",
+					"value": "100.000000000",
+					"signature": "304502204aa69afd7e10b68d9f31840711f8a9a73dd27e9afd986f340ec60c6421300886022100cedb6417ed703b5cc3d371719cdee5ebb4b0dafb240b142ea8548120201ccaea",
+					"transactionID": "4a3e2a41046c6ff24966ce6eda5f459b4c8cc67d53b3fbde167ac53c4fd6a253f335552dabc5373bdf0d020f92aa56d5"
+				}
+			],
+			"gameScoreArray": [],
+			"blockHeight": 0,
+			"blockGameHash": "1402e01a160a97a99698ba6657b0feeef3d663056e532cbc19fe8e1b37076dd12193a48528b07adaadfe0d0991847f10",
+			"blockID": "da693a17e38228d5eab8effb5be63a5ff43986aa6e8bbac4782c07ea932ed9d74d194365167d99e65930382fa632e665"
+		}
+	},
+	{
+		"blockHeight": 1,
+		"block": {
+			"transactionArray": [
+				{
+					"senderAddress": "catShipCoinBase",
+					"receiverAddress": "04a48dec00a98fc720b350a5709e5912010ed213f33c0ba7a4c5d99620b69ddd2b4d7e5f94cfe91bc2e10681a82bc6f9392a39d36cf7e0ef560604c303e5a54abc",
+					"message": "freshly minted kitty goodness",
+					"value": "100.000000000",
+					"signature": "3046022100ea98608e0c4c3c22baa66de805fee1a85c10e169edcf45fbd256e72607adb29902210091f498a347796a18ff3a88f23ab2d7e83c4082ade0fb9c57f0ead221cbf6dcc3",
+					"transactionID": "b2633899717036ee9bfe992fdc58a1489364f142df5137e17af376fe43f7e6dbf571cddd41cfe3c9f730b06ccb57f0f4"
+				},
+				{
+					"senderAddress": "04a48dec00a98fc720b350a5709e5912010ed213f33c0ba7a4c5d99620b69ddd2b4d7e5f94cfe91bc2e10681a82bc6f9392a39d36cf7e0ef560604c303e5a54abc",
+					"receiverAddress": "04f9cc17bde90abb9a366b30a6357d428843790c7ab67987558e3912c6721cd4ad75ec8ae5eebff1c87dd4a56de7ce9b08bf0f3fb7e8d454bb2a0e5104b452e284",
+					"message": "Have a coffee on me scallywag",
+					"value": "2.03",
+					"signature": "304502203a06b1e09a816b3e6a985e301cbab7238058a67d76c4d8103efb20f1993b7283022100d9a4c3b942b29b4c7c416045b7323c7e2a11a16831f01c9603e1fab902564c76",
+					"transactionID": "d3d9d525eb8c8745546ba2ac2300aedb49be9f9e72b5860778a8e3101b78c1c399d000266655bab3f1964e35b811ee70"
+				}
+			],
+			"gameScoreArray": [],
+			"blockHeight": "01",
+			"previousBlockID": "da693a17e38228d5eab8effb5be63a5ff43986aa6e8bbac4782c07ea932ed9d74d194365167d99e65930382fa632e665",
+			"blockGameHash": "45ef695ccdf81031f086802c93fbfcf237cec2a33c25cee05f4da057165701397a7d0f5cfaac0e3948987cee97ef15ae",
+			"blockID": "ea738686d7afba50b02942fe0c6e093d4925d278d8f1483b34693512fa239dda2c0d41ce79f3a7592e5e645cd788ecd5"
+		}
+	}
+];
